bc6f62bdf0cb6b22bca1cc75198d768137e637a6
sleepinglion(desktop) <admin@sleepinglion.pe.kr>
2025-03-24 03:05:27 +0900

    새 파일:       .idea/vcs.xml 	새 파일:       alembic.ini 	새 파일:       migrations/README 	새 파일:       migrations/env.py 	새 파일:       migrations/script.py.mako 	수정함:        models.py

--- models.py
+++ models.py
@@ -8,32 +8,33 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    subject = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+    name = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
 
 class Notice(Base):
-    __tablename__ = "notices"
+    __tablename__ = "notice"
 
     id = Column(Integer, primary_key=True)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
-    question_id = Column(Integer, ForeignKey("question.id"))
-    question = relationship("Question", backref="answers")
+    title = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
 
 
 class Message(Base):
     __tablename__ = "messsages"
 
     id = Column(Integer, primary_key=True)
-    subject = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+    title = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
 
 class Trainer(Base):
     __tablename__ = "trainers"
 
     id = Column(Integer, primary_key=True)
-    subject = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
