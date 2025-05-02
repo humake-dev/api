c70af5467749c18f979f13e8837a1ca12bd778a5
sleepinglion(desktop) <admin@sleepinglion.pe.kr>
2025-05-02 15:55:42 +0900

    수정함:        config/deploy.rb 	수정함:        config/deploy/production.rb 	새 파일:       lib/capistrano/tasks/poetry.rake 	새 파일:       log/capistrano.log 	수정함:        pyproject.toml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "humake_API"
+set :repo_url, "git@github.com:humake-dev/api.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/vhosts/api"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,7 +21,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, ".env"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
@@ -33,7 +33,12 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
+
+set :default_env, {
+  PATH: "$HOME/.local/bin:$PATH"
+}
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+after 'deploy:updated', 'poetry:install'
